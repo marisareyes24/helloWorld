25d2d554a18bc530e9075da30941270ece1e6036
marisareyes24 <marisa.reyes.a@gmail.com>
2024-03-01 20:06:47 -0500

    Añadir archivo helloWorld.py

--- helloWorld.py
+++ helloWorld.py
@@ -1,6 +1,7 @@
-#def welcome_message():
-name = input("Ingrese su nombre:")
-print("Bienvenidosss, ", name, " al curso de Django y React")
+def welcome_message():
 
-#if __name__ == "__main__":
- #   welcome_message  
+    nombre = input("Ingrese su nombre:")
+    print("Bienvenidosss, ", nombre, " al curso de Django y React")
+
+if __name__ == "__main__":
+    welcome_message()  
