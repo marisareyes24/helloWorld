25d1b9ccba0cf04157318c456788b0ab0f049235
marisareyes24 <marisa.reyes.a@gmail.com>
2024-03-08 21:33:59 -0500

    Corregir mensaje de bienvenida

--- helloWorld.py
+++ helloWorld.py
@@ -1,7 +1,7 @@
 def welcome_message():
 
-    nombre = input("Ingrese su nombre:")
-    print("Bienvenidos, ", nombre, " al curso de Django y React")
+    nombre = input("Ingrese su nombre: ")
+    print("Bienvenido, ", nombre, " al curso de Django y React")
 
 if __name__ == "__main__":
     welcome_message()  
