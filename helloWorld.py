cfac97eef183b657799ac7e9cf1881bb44a7301a
marisareyes24 <marisa.reyes.a@gmail.com>
2024-03-08 21:40:12 -0500

    Agregar funcionalidad de atencion

--- helloWorld.py
+++ helloWorld.py
@@ -1,6 +1,7 @@
 def welcome_message():
 
     nombre = input("Ingrese su nombre:")
+    atencion = input("Qué fue lo que más te llamó la atención del curso?")
     print("Bienvenidos, ", nombre, " al curso de Django y React")
 
 if __name__ == "__main__":
