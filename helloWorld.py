c8b05383308550aef9ed6b1744bd3edb34e15fc0
marisareyes24 <marisa.reyes.a@gmail.com>
2024-03-08 20:28:17 -0500

    Añadir archivo helloworld.py

--- helloWorld.py
+++ helloWorld.py
@@ -1,7 +1,7 @@
 def welcome_message():
 
     nombre = input("Ingrese su nombre:")
-    print("Bienvenidosss, ", nombre, " al curso de Django y React")
+    print("Bienvenidos, ", nombre, " al curso de Django y React")
 
 if __name__ == "__main__":
     welcome_message()  
